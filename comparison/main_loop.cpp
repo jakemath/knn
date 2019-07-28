29ca443b96acf992247965ecd41434558c97f83a
Jake Mathai <jakemathai5@gmail.com>
2019-07-28 03:13:45 -0700

    Create main_loop.cpp

---
+++ comparison/main_loop.cpp
@@ -0,0 +1,31 @@
+#include "routines.h"
+
+int main(int argc, char* argv[])
+{
+    std::ifstream in("KNNdata.txt"), test("KNNtest.txt"); // Read dataframe and testframe
+    list<point> dataframe, testframe;
+    load_csv(in, dataframe);
+    load_csv(test, testframe);
+    
+    // Compute distances
+    int euclid_count = 0, manhattan_count = 0, temp;
+    list<int> euclid_predictions, manhattan_predictions;
+    list<point>::iterator i = testframe.begin();
+    cout << "ACTUAL \t EUCLIDEAN \t MANHATTAN" << endl;
+    for (; i != testframe.end(); ++i)
+    {
+        cout << "\t" << i -> attributes.back() << "\t";
+        temp = knn_classify(dataframe, *i, atoi(argv[1]), sq_euclid_dist);
+        cout << temp << "\t";
+        if (temp == i -> attributes.back())
+            ++euclid_count;
+        temp = knn_classify(dataframe, *i, atoi(argv[1]), manhattan_dist);
+        cout << temp << endl;
+        if (temp == i -> attributes.back())
+            ++manhattan_count;
+    }
+    
+    cout << argv[1] << " ";
+    cout << (1.0 * euclid_count / testframe.size()) * 100 << " ";
+    cout << (1.0 * manhattan_count / testframe.size()) * 100 << endl;
+}
