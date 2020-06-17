826718879c94451281bbefdeb9e17647858e1720
Jake Mathai <jakemathai5@gmail.com>
2020-06-17 12:47:38 -0700

    Update main_driver.cpp

--- driver/main_driver.cpp
+++ driver/main_driver.cpp
@@ -15,40 +15,34 @@ int main(int argc, char* argv[])
     list<point> dataframe, testframe;
     load_csv(in, dataframe);
     load_csv(test, testframe);
-    list<point>::iterator i = dataframe.begin();
-    
+    list<point>::iterator i;
     // Print frames
     cout << "DATA FRAME:" << endl << endl;
-    for (; i != dataframe.end(); ++i)
+    for (i = dataframe.begin(); i != dataframe.end(); ++i)
         cout << *i << endl;
-    
     cout << endl << "TEST FRAME:" << endl << endl;
-    i = testframe.begin();
-    for (; i != testframe.end(); ++i)
+    for (i = testframe.begin(); i != testframe.end(); ++i)
         cout << *i << endl;
-   
     // Compute distances and report predictions
-    int euclid_count = 0, manhattan_count = 0, temp;
+    int euclid_count = 0, manhattan_count = 0, euclid_predict, manhattan_predict;
     list<int> euclid_predictions, manhattan_predictions;
-    list<point>::iterator i = testframe.begin();
     cout << "ACTUAL \t EUCLIDEAN \t MANHATTAN" << endl;
-    for (; i != testframe.end(); ++i)
+    for (i = testframe.begin(); i != testframe.end(); ++i)
     {
         cout << "\t" << i -> attributes.back() << "\t";
-        temp = knn_classify(dataframe, *i, atoi(argv[1]), sq_euclid_dist);
-        cout << temp << "\t";
-        if (temp == i -> attributes.back())
+        euclid_predict = knn_classify(dataframe, *i, atoi(argv[1]), sq_euclid_dist);
+        cout << euclid_predict << "\t";
+        if (euclid_predict == i -> attributes.back())
             ++euclid_count;
-        temp = knn_classify(dataframe, *i, atoi(argv[1]), manhattan_dist);
-        cout << temp << endl;
-        if (temp == i -> attributes.back())
+        manhattan_predict = knn_classify(dataframe, *i, atoi(argv[1]), manhattan_dist);
+        cout << manhattan_predict << endl;
+        if (manhattan_predict == i -> attributes.back())
             ++manhattan_count;
     }
-    
     // Report accuracy
-    cout << endl << "Euclid accuracy: ";
-    cout << (1.0 * euclid_count / euclid_predictions.size()) * 100;
-    cout << "%" << endl << "Manhattan accuracy: ";
-    cout << (1.0 * manhattan_count / euclid_predictions.size()) * 100 << "%";
+    cout << "Euclid correct: " << euclid_count << ", ";
+    cout << (1.0 * euclid_count / testframe.size()) * 100 << "%" << endl;
+    cout << "Manhattan correct: " << manhattan_count << ", ";
+    cout << (1.0 * manhattan_count / testframe.size()) * 100 << "%";
     cout << endl << endl;
 }
